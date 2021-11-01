cec61f1537834c0a7f8f2485853ad2f984a24df4
vernellmiller <nezeey@gmail.com>
2021-11-01 17:26:08 -0400

    adds player schema

---
+++ models/player.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const showSchema = new Schema({
+    firstName: { type: String },
+    lastName: { type: String},
+    position: { type: String},
+    heightFeet: { type: String},
+    heightInches: { type: String},
+    weight: { type: String}
+}, { timestamps: true });
+
+module.exports = mongoose.model("Player", showSchema);
